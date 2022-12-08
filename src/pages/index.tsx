c881f4b306f8ef9b6d6da66c86ffcffba98d740d
Earl Tacogdoy <earltacogdoy@gmail.com>
2022-12-08 22:54:52 +0800

    #2 - add tailwindcss - Added tailwindcss - Refractored all default css to using tailwindcss apply

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Cards from '../components/Cards/Cards'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
